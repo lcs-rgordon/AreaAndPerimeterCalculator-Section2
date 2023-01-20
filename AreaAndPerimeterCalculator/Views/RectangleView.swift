7b5642ee64fe22f832d42a5307acaabbdbd78cf8
Russell Gordon <rgordon@lcs.on.ca>
2023-01-20 14:44:04 -0500

    Calculates area (not perimeter) but has stepper to control precision (after lessons from days 5 and 6).

--- AreaAndPerimeterCalculator/Views/RectangleView.swift
+++ AreaAndPerimeterCalculator/Views/RectangleView.swift
@@ -10,8 +10,12 @@ import SwiftUI
 struct RectangleView: View {
     
     // MARK: Stored properties
-    let length: Double = 100
-    let width: Double = 5
+    // @State is a "property wrapper" that essentially
+    // tells SwiftUI to update the user interface when these
+    // values change.
+    @State var length: Double = 50
+    @State var width: Double = 25
+    @State var desiredPrecision: Double = 1
     
     // MARK: Computed properties
     var area: Double {
@@ -26,35 +30,74 @@ struct RectangleView: View {
                 Image("Rectangle")
                     .resizable()
                     .scaledToFit()
-                .frame(width: 250)
+                    .frame(width: 250)
                 
                 Spacer()
             }
             
-            Text("Length")
-                .font(.title2)
-                .bold()
-            
-            // Use string interpolation to convert
-            // the Double data type to text (String)
-            //
-            // \(variable)
-            Text("\(length)")
-                .font(.title2)
+            Group {
 
-            Text("Width")
-                .font(.title2)
-                .bold()
-            
-            Text("\(width)")
-                .font(.title2)
+                Text("Length")
+                    .font(.title2)
+                    .bold()
 
-            Text("Area")
-                .font(.title2)
-                .bold()
+                Slider(value: $length,
+                       in: 0...100,
+                       label: { Text("Length") },
+                       minimumValueLabel: { Text("0") },
+                       maximumValueLabel: { Text("100") })
+                
+                // Use string interpolation to convert
+                // the Double data type to text (String)
+                //
+                // \(variable)
+                Text("\(length.formatted(.number.precision(.fractionLength(Int(desiredPrecision)))))")
+                    .font(.title2)
+
+            }
+                        
+            Group {
+                Text("Width")
+                    .font(.title2)
+                    .bold()
+                
+                Slider(value: $width,
+                       in: 0...100,
+                       label: { Text("Width") },
+                       minimumValueLabel: { Text("0") },
+                       maximumValueLabel: { Text("100") })
+                
+                
+                Text("\(width.formatted(.number.precision(.fractionLength(Int(desiredPrecision)))))")
+                    .font(.title2)
+            }
             
-            Text("\(area)")
-                .font(.title2)
+            Group {
+                Text("Area")
+                    .font(.title2)
+                    .bold()
+
+                Text("\(area.formatted(.number.precision(.fractionLength(Int(desiredPrecision)))))")
+                    .font(.title2)
+
+            }
+            
+            Group {
+                Text("Perimeter")
+                    .font(.title2)
+                    .bold()
+            }
+            
+            // Allow user to control precision
+            Group {
+                
+                Stepper("Precision", value: $desiredPrecision, in: 0...6)
+                
+                // Show precision
+                Text("\(desiredPrecision.formatted(.number.precision(.fractionLength(0))))")
+                    .font(.title2)
+                
+            }
             
             Spacer()
             
